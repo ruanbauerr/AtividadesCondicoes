c07578a10e5e1556df3f455b2c6ec6f81361c5eb
ruanbauerr <ruanbauer4@gmail.com>
2024-08-30 08:09:26 -0300

    Atividade 4 feita

---
+++ src/VerificaçãodeLogin.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class VerificaçãodeLogin {
+    public static void main(String[] args) {
+     String usuarioCorreto = "admin";
+     String senhaCorreta = "admin";
+        Scanner entrada = new Scanner(System.in);
+        System.out.print("Usuario: ");
+        String usuarioInserido = entrada.nextLine();
+
+        System.out.print("Senha: ");
+        String senhaInserida = entrada.nextLine();
+        if (usuarioInserido.equals(usuarioCorreto) && senhaInserida.equals(senhaCorreta)) {
+            System.out.println("Login bem sucedido!");
+        } else {
+            System.out.println("Tente novamente.");
+        }
+
+    }
+}
